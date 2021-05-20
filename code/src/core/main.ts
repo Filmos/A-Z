270455ff9dce02a802fb82adcc771b0406a751c7
Filmos <git@filmos.net>
2021-05-20 17:42:23 +0200

    Added base for future reflection

--- code/src/core/main.ts
+++ code/src/core/main.ts
@@ -1,5 +1,10 @@
 class TimeBlock extends DescriptiveObject {
 
+  @_() private _firstName: string;
+  @_() private _int: number;
+  @_() private _circle: TimeBlock;
+  @_() private _class: DescriptiveField;
+
   constructor(name: string, timeslot: string) {
     super();
     this.registerField("name", new fields["title"](name), name)
@@ -7,7 +12,19 @@ class TimeBlock extends DescriptiveObject {
   }
 }
 
+function _() {
+  return function (target: any, propertyKey: string) {
+    // @ts-ignore
+    let R = Reflect.getMetadata("design:type", target, propertyKey)
+    target[propertyKey] = 13
+    console.log(propertyKey, R.prototype)
+  };
+}
+
 let block = new TimeBlock("Hello", "there")
 let block2 = new TimeBlock("fellow", "human")
 
 document.body.appendChild(DescriptiveUI.singleForm(block))
+
+console.log(TimeBlock.prototype)
+console.log(block)
