0c023f1f87dbd8905390064570a37cbaf3648c37
Filmos <git@filmos.net>
2021-07-04 22:35:57 +0200

    Added transform-based mapping support for arrays and generics

--- code/transformers/mapping.ts
+++ code/transformers/mapping.ts
@@ -1,8 +1,26 @@
-//TODO: make it work for getters
-//TODO: make it work for arrays
-//TODO: add subtype comments
+//TODO add subtype comments
+//TODO remove blank decorator leftover
 import * as ts from 'typescript';
 let factory = ts.factory
+
+function getTypeDefinition(type) {
+    let tokenTranslation = {
+        131: "booolean",
+        144: "number",
+        147: "string",
+        178: "Array"
+    }
+    let propertyType = tokenTranslation[type.kind] || type.kind
+    if(type.typeName) propertyType = type.typeName.escapedText
+    propertyType = [factory.createStringLiteral(""+propertyType)]
+
+    if(type.elementType) propertyType.push(getTypeDefinition(type.elementType))
+    if(type.typeArguments)
+        for(let arg of type.typeArguments) propertyType.push(getTypeDefinition(arg))
+
+    return factory.createArrayLiteralExpression(propertyType)
+}
+
 export default function(program: ts.Program, pluginOptions: any) {
     return (ctx: ts.TransformationContext) => {
         return (sourceFile: ts.SourceFile) => {
@@ -13,6 +31,7 @@ export default function(program: ts.Program, pluginOptions: any) {
                 if (ts.isClassDeclaration(node)) {
                     // Enter property level
                     for(let subnode of node.members) {
+                        // @ts-ignore
                         if(subnode.name && subnode.name.escapedText == mapVariableName) {
                             mapValues = null
                             break
@@ -30,25 +49,19 @@ export default function(program: ts.Program, pluginOptions: any) {
                         }
                         if(!isDescriptive) continue
 
-                        // @ts-ignore
-                        let type = subnode.type
-                        let tokenTranslation = {
-                            131: "booolean",
-                            144: "number",
-                            147: "string"
-                        }
-                        let propertyType = tokenTranslation[type.kind] || type.kind
-                        if(type.typeName) propertyType = type.typeName.escapedText
                         mapValues.push(
                             factory.createPropertyAssignment(
+                                // @ts-ignore
                                 subnode.name.escapedText,
-                                factory.createStringLiteral(propertyType)
+                                // @ts-ignore
+                                getTypeDefinition(subnode.type)
                             )
                         )
                     }
 
                     if(mapValues) {
                         let mapProperty = factory.createPropertyDeclaration([], [ts.createModifier(ts.SyntaxKind.StaticKeyword)], mapVariableName, undefined, undefined, factory.createObjectLiteralExpression(mapValues))
+                        // @ts-ignore
                         return factory.updateClassDeclaration(node, node.decorators, node.modifiers, node.name.escapedText, node.typeParameters, node.heritageClauses, [mapProperty, ...node.members])
                     }
                 }
