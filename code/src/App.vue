5a1df65df9d1e44b14f7d81852529404a0a7f7ed
Filmos <git@filmos.net>
2022-01-03 11:50:01 +0100

    Reimported app layout

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Frame><div class="temp"/></Frame>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+    import Vue from 'vue';
+    import Frame from './components/Frame.vue';
 
-export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+    export default Vue.extend({
+      name: 'App',
+      components: {
+        Frame
+      }
+    });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+
+    .temp {
+        height: 2rem;
+        background-color: dodgerblue;
+    }
 </style>
---
+++ code/src/components/Frame.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="frame">
+        <svg class="top-shrinker">
+            <polygon v-for="i in 100" :key="i" :points="`${i*30-7},0 ${i*30},12 ${i*30+7},0`" v-on:click="setWidth(i)"></polygon>
+        </svg>
+        <div class="liveline" />
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+
+    export default Vue.extend({
+        name: 'Frame',
+        methods: {
+            setWidth: function(i: number) {(this.$el as HTMLElement).style.maxWidth = (i*30-24)+"px"}
+        }
+    });
+</script>
+
+<style scoped>
+    .liveline {
+        height: 2rem;
+        background-color: green;
+    }
+
+    .frame {
+        padding: 12px;
+    }
+
+    .top-shrinker {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 16px;
+        fill: #0d1021;
+        opacity: 0;
+        transition: opacity 0.5s;
+    }
+
+    .top-shrinker:hover {
+        opacity: 0.8;
+    }
+
+    .top-shrinker polygon {
+        opacity: 0.5;
+    }
+
+    .top-shrinker polygon:hover {
+        opacity: 1;
+    }
+</style>
+<style>
+    body {
+        font-family: "EncodeSans",sans-serif;
+        margin: 0;
+        border: 0 solid transparent;
+        background-image: radial-gradient(circle at 0% 0%, #373b52, #252736 51%, #1d1e26);
+        background-attachment: fixed;
+    }
+</style>
