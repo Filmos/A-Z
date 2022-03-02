9b487c01897f73eab959f73ecd022db9c214b8b9
Filmos <git@filmos.net>
2022-03-02 16:20:48 +0100

    Added command processing to liveline

--- code/src/components/liveline/Liveline.vue
+++ code/src/components/liveline/Liveline.vue
@@ -2,7 +2,7 @@
     <div class="liveline">
         <input @input="(e) => update(e.target.value)" type="text" name="text" autocomplete="off" :class="Object.values(command).some(a=>!properArg(a))?'invalid':''">
         <div class="notes">
-            <span v-for="(arg, argName) in command" :key="argName" :class="properArg(arg)?'':'invalid'"> {{ argName+": "+(arg.display || "&lt;missing&gt;") }} </span>
+            <span v-for="(arg, argName) in command" :key="argName" :class="properArg(arg)?'':'invalid'"> {{ argName+": "+displayArg(arg) }} </span>
         </div>
     </div>
 </template>
@@ -14,21 +14,78 @@
         name: 'Liveline',
         methods: {
             properArg(arg) {
-                return !!arg.display
+                return !!arg.val
+            },
+            displayArg(arg) {
+                console.log(arg)
+                if (!arg.raw) return "<missing>"
+                let ret = arg.display(arg.val)
+                if (!ret) return "<invalid>"
+                return ret
             },
             update(command) {
+                function parseDate(val) {
+                    if (!val) return
+                    let reg = val.match(/^(next)? ?(?:(Su)|(M)|(Tu)|(W)|(Th)|(F)|(Sa))/i)
+                    if (reg) {
+                        let day = 0
+                        for (let i = 0; i < 7; i++) {
+                            if (reg[i + 2]) { day = i; break }
+                        }
+                        let time = ((day - (new Date()).getDay() + 7) % 7) || 7
+                        if (reg[1]) time += 7
+
+                        let date = new Date()
+                        date.setDate(date.getDate() + time)
+                        date.setHours(12, 0, 0, 0)
+                        return date
+                    }
+
+                    reg = val.match(/(\d\d?)(?:.| -)(\d\d?)/i)
+                    if (reg) {
+                        let date = new Date()
+                        date.setMonth(parseInt(reg[2]) - 1)
+                        date.setDate(parseInt(reg[1]))
+                        date.setHours(12, 0, 0, 0)
+                        return date
+                    }
+                }
+                function parseDuration(val) {
+                    if (!val) return
+                    let pat = val.match(/(?:(\d+) ?(?:w(?:e(?:e(?:k(?:s)?)?)?)?))? ?\+? ?(?:(\d+) ?(?:d(?:a(?:y(?:s)?)?)?)?)?/i)
+                    if(!pat[1] && !pat[2]) return
+
+                    let days = 0
+                    days += (pat[1] || 0) * 7
+                    days += (pat[2] || 0) * 1
+
+                    return days
+                }
+
+                function displayDate(val: Date) {
+                    if (!val) return
+                    let time = Math.ceil((val.getTime() - (new Date()).getTime()) / 1000 / 60 / 60 / 24)
+                    let specialNames = {"-1": "Yesterday", "0": "Today", "1": "Tomorrow"}
+                    return (specialNames[time+""] || ("In " + time + " days")) + " (" + (val.getDate()+"").padStart(2, "0") + "." + ((val.getMonth() + 1)+"").padStart(2, "0") + ")"
+
+                }
+                function displayDuration(val) {
+                    if(!val) return
+                    return val + " day" + (val>1?"s":"")
+                }
+
                 this.command = {}
                 if (!command) return
 
                 let args = command.split(";").map(s => s.trim())
-                this.command["Task"] = { val: args[0], display: args[0] }
-                this.command["Deadline"] = { val: args[1], display: args[1] }
+                this.command["Task"] = { raw: args[0], val: args[0], display: v=>v }
+                this.command["Deadline"] = { raw: args[1], val: parseDate(args[1]), display: displayDate }
 
                 if (args.length < 3) return
-                this.command["Heads-up"] = { val: args[2], display: args[2] }
+                this.command["Heads-up"] = { raw: args[2], val: parseDuration(args[2]), display: displayDuration }
 
                 if (args.length < 4) return
-                this.command["Repeats"] = { val: args[3], display: args[3] }
+                this.command["Repeats"] = { raw: args[3], val: parseDuration(args[3]), display: displayDuration }
             }
         },
         data() {
