55f03b055fdbe2a8bb5299060026e88f6ec40f9d
Filmos <git@filmos.net>
2022-03-03 17:43:54 +0100

    Added minified task info

--- code/src/components/tasks/Tile.vue
+++ code/src/components/tasks/Tile.vue
@@ -1,7 +1,7 @@
 <template>
     <g @click="clickEvent" transform-origin="50 6" :priority="priority">
         <path d="M 50 6 L 72 28 L 50 50 L 28 28 L 50 6" />
-        <text x="50" y="30" dominant-baseline="middle" text-anchor="middle">{{ title }}</text>
+        <text x="50" y="30" dominant-baseline="middle" text-anchor="middle">{{ shortTitle }}</text>
     </g>
 </template>
 
@@ -9,7 +9,7 @@
     import Vue from 'vue';
     import { ref, remove } from "firebase/database";
     import db from '@/core/database';
-    import { multiClick } from '@/core/helper';
+    import { multiClick, randomColorFromString } from '@/core/helper';
 
     export default Vue.extend({
         name: 'Tile',
@@ -18,8 +18,8 @@
         },
         data() {
             return {
-                mycolor: '#' + (Math.random() * 0xFFFFFF << 0).toString(16) + "44",
-                clickEvent: multiClick(1000, (clicks) => {
+                mycolor: randomColorFromString(this.dbKey) + "44",
+                clickEvent: multiClick(300, (clicks) => {
                     if (clicks != 2) return
                     const dbRef = ref(db, 'tasks/' + this.dbKey)
                     remove(dbRef)
@@ -28,6 +28,22 @@
         },
         mounted() {
             (this["$el"].children[0] as HTMLElement).style.fill = this.mycolor;
+        },
+        computed: {
+            shortTitle() {
+                let reg = this.title.match(/(?:^|\s)([A-Z]{2,3})(?:^|\s)/)
+                if (reg) return reg[1]
+
+                let phrase = this.title as string
+                reg = this.title.match(/(?:^|\s)(\w{4,})(?:$|\s)/i)
+                if (reg) phrase = reg[1]
+
+                if (phrase.replace(/[\W]/gi, '').length > 1) phrase = phrase.replace(/[\W]/gi, '')
+                if (phrase.replace(/[aioue]/gi, '').length > 1) phrase = phrase.replace(/[aioue]/gi, '')
+                if (phrase.length < 2) phrase += "--"
+
+                return phrase[0].toUpperCase()+phrase[1].toLowerCase()
+            }
         }
     });
 </script>
@@ -39,6 +55,12 @@
 
         path {
             fill: rgba(255, 0, 0, 0.3);
+        }
+
+        text {
+            cursor: default;
+            user-select: none;
+            pointer-events: none;
         }
     }
 </style>
--- code/src/core/helper.ts
+++ code/src/core/helper.ts
@@ -10,7 +10,7 @@ export function sortedIndex<ArrayType, ValueType>(array: ArrayType[], value: Val
     return low;
 }
 
-export function multiClick(maxDelay: number, func: (clicks: number, ...args: any[])=>void) {
+export function multiClick(maxDelay: number, func: (clicks: number, ...args: any[]) => void): (...args: any[]) => void {
     let lastClick = 0
     let clickCount = 0
 
@@ -22,4 +22,18 @@ export function multiClick(maxDelay: number, func: (clicks: number, ...args: any
         return func(clickCount, ...passThrough)
     }
     return ret
+}
+
+export function hashString(input: string): number {
+    let hash = 0, i, chr
+    if (input.length === 0) return hash
+    for (i = 0; i < input.length; i++) {
+        chr = input.charCodeAt(i)
+        hash = ((hash << 5) - hash) + chr
+        hash |= 0
+    }
+    return hash
+}
+export function randomColorFromString(input: string): string {
+    return "#" + (Math.abs(hashString(input)) % (256 ** 3)).toString(16)
 }
